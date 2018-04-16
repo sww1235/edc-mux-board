eec47d96ba2309e2ce3243cc4c10fd1bc385ba3d
sww1235 <sww1235@gmail.com>
2018-04-15 19:37:58 -0600

    further mixer work

--- code/vhdl/fullmixer.vhdl
+++ code/vhdl/fullmixer.vhdl
@@ -3,7 +3,7 @@
 
 entity fullmixer is   -- 24x24 mixer matrix: 8 R IO, 8 L IO 8 mic IO
   port (i : in array(23 downto 0) of bit_vector(15 downto 0);
-        o : in array(23 downto 0) of bit_vector(15 downto 0));
+        o : out array(23 downto 0) of bit_vector(15 downto 0));
 
 
 end fullmixer
---
+++ code/vhdl/mixer.vhdl
@@ -0,0 +1,12 @@
+
+entity mixer is   -- 8x1 mixer 
+  port (i : in array(8 downto 0) of bit_vector(15 downto 0);
+        o : out bit_vector(15 downto 0));
+
+
+end mixer
+
+architecture arch of mixer is
+
+
+end arch
