4a4a21bc9ae1e1a3f1dc57bb6b6f6e7034dc28d5
sww1235 <sww1235@gmail.com>
2018-04-17 16:25:27 -0600

    started 8-1 mux

---
+++ code/vhdl/8-1-mux.vhdl
@@ -0,0 +1,16 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity mux8_1 is
+  port (i : in std_logic_vector(7 downto 0);
+        o : out std_logic;
+        ctl : in std_logic_vector(2 downto 0));
+
+end mux8_1;
+
+
+architecture arch of mux8_1 is
+
+begin
+
+end arch;
