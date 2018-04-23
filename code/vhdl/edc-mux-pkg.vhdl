d25f0c912c983960e48dad13cedbf842e428ec18
sww1235 <sww1235@gmail.com>
2018-04-23 17:03:38 -0600

    added additional data type

--- code/vhdl/edc-mux-pkg.vhdl
+++ code/vhdl/edc-mux-pkg.vhdl
@@ -8,4 +8,5 @@ package edc_mux_pkg is
   type port_t is array (31 downto 0) of SIGNED(15 downto 0); -- port type for IO ports
   type ctl_port_array_t is array (31 downto 0) of port_t;
   type buffer_t is array (31 downto 0) of SIGNED(31 downto 0);
+  type i2s_port_t is array (15 downto 0) of std_logic; -- i2s data lines
 end package;
