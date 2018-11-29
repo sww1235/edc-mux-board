5952bd952a7182748d24d84a557a989335606bad
sww1235 <sww1235@gmail.com>
2018-11-29 16:23:15 -0700

    added clk sensitivity and added comments

--- code/vhdl/fullmixer.vhdl
+++ code/vhdl/fullmixer.vhdl
@@ -6,9 +6,10 @@ use ieee.numeric_std.all;
 use work.edc_mux_pkg.all;
 
 entity fullmixer is   -- 32x32 mixer matrix: 8 R IO, 8 L IO 16 mic IO
-  port (i     : in  port_t;
-        o     : out port_t;
-        ctl   : in  ctl_port_array_t);
+  port (i     : in  port_t; -- 32 size array of 16 bit signed values
+        o     : out port_t; -- 32 size array of 16 bit signed values
+        ctl   : in  ctl_port_array_t
+        clk   : in std_logic); -- 32 size array of (32 size array of 16 bit signed values)
 
 
 
@@ -20,21 +21,27 @@ use ieee.numeric_std.all;
 use work.edc_mux_pkg.all;
 
 architecture Algorithmic of fullmixer is
-  signal vo : buffer_t;
+  -- buffer for output allowing for truncation
+  signal vo : buffer_t; -- 32 size array of 32 bit signed values
 
 begin
-  process(i, ctl)
+  process(clk) -- want process to be sensitive to clock signal
     begin
-      for J in 0 to 31 loop
-        for K in 0 to 31 loop
+      if rising_edge(clk) then
+        -- loop through output buffer array
+        for J in 0 to 31 loop
+        -- loop through each input and multiply it by the control signal
+        -- then add it to the current buffer output
+          for K in 0 to 31 loop
 
-          vo(J) <= vo(J) + ((i(J) * ctl(J)(K)));
+            vo(J) <= vo(J) + ((i(J) * ctl(J)(K)));
+
+          end loop;
+          -- truncate buffer and insert into output register
+          o(J) <= vo(J)(23 downto 8);
 
         end loop;
-
-        o(J) <= vo(J)(23 downto 8);
-
-      end loop;
+      end if;
     end process;
 
 end Algorithmic;
