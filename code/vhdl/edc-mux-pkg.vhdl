ae691581ea0731aa5bf5e3a82cc835f28c418309
sww1235 <sww1235@gmail.com>
2018-12-15 15:10:44 -0700

    fixing control bitwidth issues

--- code/vhdl/edc-mux-pkg.vhdl
+++ code/vhdl/edc-mux-pkg.vhdl
@@ -7,7 +7,7 @@ use ieee.numeric_std.all;
 package edc_mux_pkg is
   subtype audio_buffer_t is SIGNED(15 downto 0); -- audio buffer type for i2s interface
   type audio_port_t is array (31 downto 0) of audio_buffer_t; -- port type for audio IO ports
-  type ctl_port_array_t is array (31 downto 0) of audio_port_t;  -- signed numbers to multiply by
+  type ctl_port_array_t is array (31 downto 0) of SIGNED(7 downto 0);  -- signed numbers to multiply by
   type mix_buffer_t is array (31 downto 0) of SIGNED(31 downto 0); -- mixer summing buffers
   type i2s_port_t is array (15 downto 0) of std_logic; -- i2s data lines
 end package;
