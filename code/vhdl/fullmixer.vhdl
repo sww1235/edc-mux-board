7231265fcec9a4a31ffaf084b4dfeb4e1943041f
sww1235 <sww1235@gmail.com>
2019-01-20 16:19:27 -0700

    switched mixer logic to be unclocked added clocked signal assignment w/ buffer

--- code/vhdl/fullmixer.vhdl
+++ code/vhdl/fullmixer.vhdl
@@ -26,25 +26,37 @@ use work.edc_mux_pkg.all;
 architecture Algorithmic of fullmixer is
   -- buffer for output allowing for truncation
   signal vo : mix_buffer_t; -- 32 size array of 32 bit signed values
+  signal iBuff : audio_port_t;
 
 begin
   process(clk) -- want process to be sensitive to clock signal
     begin
-      if rising_edge(clk) then
-        -- loop through output buffer array
-        for J in 0 to 31 loop
-        -- loop through each input and multiply it by the control signal
-        -- then add it to the current buffer output
-          for K in 0 to 31 loop
+      if (rising_edge(clk) and (or in_audio_ready = '1') )then -- any audio signal is ready
+        -- assign audio inputs
 
-            vo(J) <= vo(J) + ((i(J) * ctl(J)(K))); -- signed so only 127 volume steps
+        for each_out in 0 to 15 loop
+        if (in_audio_ready(each_out) = '1') and (in_lr_audio_strobe(each_out) = '0') then -- channel 0 (left)
+          iBuff(each_out*2) <= i(each_out*2);
+        end if;
 
-          end loop;
-          -- truncate buffer and insert into output register
-          o(J) <= vo(J)(23 downto 8);
+        if (in_audio_ready(each_out) = '1') and (in_lr_audio_strobe(each_out) = '1') then -- channel 1 (right)
+          iBuff((each_out*2)+1) <= i((each_out*2)+1);
+        end if;
 
-        end loop;
+      end loop;
+
       end if;
     end process;
+
+-- this doesn't need to be clocked. Should be directly implemented in logic
+  out_loop: for J in 0 to 31 generate
+    -- loop through each input and multiply it by the control signal
+    -- then add it to the current buffer output
+    in_loop: for K in 0 to 31 generate
+      vo(J) <= vo(J) + ((iBuff(J) * ctl(J)(K))); -- signed so only 127 volume steps
+    end generate;
+      -- truncate buffer and insert into output register
+      o(J) <= vo(J)(23 downto 8);
+  end generate;
 
 end Algorithmic;
