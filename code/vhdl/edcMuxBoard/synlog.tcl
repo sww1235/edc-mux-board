6dfaa475dbdb961812f844a8746c5022b728c55b
Stephen Walker-Weinshenker <sww1235@users.noreply.github.com>
2019-01-22 19:48:02 -0700

    Update synlog.tcl

--- code/vhdl/edcMuxBoard/synlog.tcl
+++ code/vhdl/edcMuxBoard/synlog.tcl
@@ -1,1 +1,45 @@
+history clear
 project -load edcMuxBoard_syn.prj
+text_select 244 5 244 26
+text_select 143 1 144 1
+project -run  
+project -run  
+project -run  
+project -run  
+project -run  
+set_option -vhdl2008 1
+text_select 19 8 19 15
+set_option -top_module edc_mux
+project -save C:/Users/toxic/Documents/GitHub/edc-mux-board/code/vhdl/edcMuxBoard/edcMuxBoard_syn.prj 
+project -run  
+text_select 61 37 61 39
+text_select 61 31 61 38
+project -run  
+project -run  
+project -run  
+project -run  
+text_select 29 59 29 71
+project -run  
+text_select 144 40 144 45
+text_select 156 25 156 29
+text_select 159 26 159 31
+text_select 159 26 159 30
+text_select 162 50 162 56
+project -run  
+project -run  
+project -run  
+project -run  
+text_select 34 36 34 50
+project -run  
+project -run  
+project -run  
+project -run  
+text_select 144 7 152 49
+project -run  
+set_option -frequency 1.000000
+set_option -frequency 48
+project -save C:/Users/toxic/Documents/GitHub/edc-mux-board/code/vhdl/edcMuxBoard/edcMuxBoard_syn.prj 
+project -run  
+project -run  
+project -run  
+project -close C:/Users/toxic/Documents/GitHub/edc-mux-board/code/vhdl/edcMuxBoard/edcMuxBoard_syn.prj
