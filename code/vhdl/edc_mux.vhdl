5d500b60652e07efca567c139b9ea030ccd6700c
sww1235 <sww1235@gmail.com>
2019-02-17 17:33:50 -0700

    audio ctl reg type swap

--- code/vhdl/edc_mux.vhdl
+++ code/vhdl/edc_mux.vhdl
@@ -197,8 +197,8 @@ architecture arch of edc_mux is
 
 								aud_out_sel <= to_integer(unsigned(instruction1(4 downto 0))); -- which output channel
 								aud_in_sel <= to_integer(unsigned(instruction2(4 downto 0))); -- which input channel on that output channel
-								-- volume level is signed so only 127 volume steps. Leave MSB 0 always.
-								audio_ctl_reg(aud_out_sel)(aud_in_sel) <= signed(instruction3); -- volume level of input channel in output channel
+								-- volume level is integer restricted to 0 - 255. Need to append 0 MSB in order to get right bit width integer
+								audio_ctl_reg(aud_out_sel)(aud_in_sel) <= to_integer(signed('0' & instruction3)); -- volume level of input channel in output channel
 
 							when "01" => null;
 							when "10" => -- Select inputs that control outputs.
