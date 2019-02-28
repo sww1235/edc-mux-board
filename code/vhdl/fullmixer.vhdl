771604f0f7c5243ad638a77220f22c1e780b9c6a
Stephen Walker-Weinshenker <sww1235@users.noreply.github.com>
2019-02-28 00:30:54 -0700

    reduced case statement with if statement and direct assignment

--- code/vhdl/fullmixer.vhdl
+++ code/vhdl/fullmixer.vhdl
@@ -70,105 +70,12 @@ end component mixerChannel;
 
 			mixerStateSelect : process(all)
 			begin
-				case(counter) is
-					when 0 =>
-						o(0) <= channelout;
-						channelctl <= ctl(0);
-					when 1 =>
-						o(1) <= channelout;
-						channelctl <= ctl(1);
-					when 2 =>
-						o(2) <= channelout;
-						channelctl <= ctl(2);
-					when 3 =>
-						o(3) <= channelout;
-						channelctl <= ctl(3);
-					when 4 =>
-						o(4) <= channelout;
-						channelctl <= ctl(4);
-					when 5 =>
-						o(5) <= channelout;
-						channelctl <= ctl(5);
-					when 6 =>
-						o(6) <= channelout;
-						channelctl <= ctl(6);
-					when 7 =>
-						o(7) <= channelout;
-						channelctl <= ctl(7);
-					when 8 =>
-						o(8) <= channelout;
-						channelctl <= ctl(8);
-					when 9 =>
-						o(9) <= channelout;
-						channelctl <= ctl(9);
-					when 10 =>
-						o(10) <= channelout;
-						channelctl <= ctl(10);
-					when 11 =>
-						o(11) <= channelout;
-						channelctl <= ctl(11);
-					when 12 =>
-						o(12) <= channelout;
-						channelctl <= ctl(12);
-					when 13 =>
-						o(13) <= channelout;
-						channelctl <= ctl(13);
-					when 14 =>
-						o(14) <= channelout;
-						channelctl <= ctl(14);
-					when 15 =>
-						o(15) <= channelout;
-						channelctl <= ctl(15);
-					when 16 =>
-						o(16) <= channelout;
-						channelctl <= ctl(16);
-					when 17 =>
-						o(17) <= channelout;
-						channelctl <= ctl(17);
-					when 18 =>
-						o(18) <= channelout;
-						channelctl <= ctl(18);
-					when 19 =>
-						o(19) <= channelout;
-						channelctl <= ctl(19);
-					when 20 =>
-						o(20) <= channelout;
-						channelctl <= ctl(20);
-					when 21 =>
-						o(21) <= channelout;
-						channelctl <= ctl(21);
-					when 22 =>
-						o(22) <= channelout;
-						channelctl <= ctl(22);
-					when 23 =>
-						o(23) <= channelout;
-						channelctl <= ctl(23);
-					when 24 =>
-						o(24) <= channelout;
-						channelctl <= ctl(24);
-					when 25 =>
-						o(25) <= channelout;
-						channelctl <= ctl(25);
-					when 26 =>
-						o(26) <= channelout;
-						channelctl <= ctl(26);
-					when 27 =>
-						o(27) <= channelout;
-						channelctl <= ctl(27);
-					when 28 =>
-						o(28) <= channelout;
-						channelctl <= ctl(28);
-					when 29 =>
-						o(29) <= channelout;
-						channelctl <= ctl(29);
-					when 30 =>
-						o(30) <= channelout;
-						channelctl <= ctl(30);
-					when 31 =>
-						o(31) <= channelout;
-						channelctl <= ctl(31);
-
-				end case;
+				if outSel < 32 then -- 0 to 31
+					o(outSel) <= channelout;
+					channelctl <= ctl(outSel);
+				else
+					null; -- 32 to 39
+				end if;
 			end process;
 
 			channel : mixerChannel
